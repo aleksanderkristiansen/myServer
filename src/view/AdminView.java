d07d687f673e0100faf46e510208e7b2dc35bc6d
Aleksander <alek.rk@gmail.com>
2016-11-25 06:39:23 +0100

    sotr

--- src/view/AdminView.java
+++ src/view/AdminView.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import config.Config;
 import config.ConfigMap;
 import controllers.BookController;
+import controllers.UserController;
 import model.Book;
+import model.User;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,16 +17,14 @@ import java.util.Scanner;
  */
 public class AdminView {
 
-    public AdminView(){
+    Scanner sc = new Scanner(System.in);
 
+    UserController userController = new UserController();
+    BookController bookController = new BookController();
 
-    }
+    public AdminView(){}
 
     public void run() throws Exception {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Hej");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("./config.json")));
 
@@ -38,13 +38,6 @@ public class AdminView {
         Config.setDbUrl(config.getDbUrl());
         Config.setDbUserName(config.getDbUserName());
 
-        BookController bookController = new BookController();
-
-
-        for (Book book : bookController.getBooks()){
-            System.out.println(book.getTitle());
-        }
-
         System.out.print(" 1: Vis alle brugere"
                 +"\n 2: Opret bruger"
                 +"\n 3: Opret Bog"
@@ -53,18 +46,45 @@ public class AdminView {
 
         int choice = sc.nextInt();
 
-        switch (choice){
-            case 1: showAllUsers();
-                break;
-            case 2: createUser();
-                break;
-            case 3: createBOok();
-                break;
-            case 4: deleteUser();
-                break;
-            case 5: deleteBook();
-                break;
+        boolean stop = true;
+
+        while (stop){
+            switch (choice){
+                case 1: showAllUsers();
+                    break;
+                case 2: createUser();
+                    break;
+                case 3: createBook();
+                    break;
+                case 4: deleteUser();
+                    break;
+                case 5: deleteBook();
+                    break;
+            }
         }
 
+
+
+    }
+
+    private void deleteUser(){
+
+    }
+
+    private void deleteBook(){
+
+    }
+
+    private void createBook() {
+    }
+
+    private void createUser() {
+    }
+
+
+    private void showAllUsers() {
+        for (User user : userController.getUsers()){
+            System.out.println(user.getEmail());
+        }
     }
 }
